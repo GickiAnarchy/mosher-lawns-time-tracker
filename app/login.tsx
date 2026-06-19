e1b71e49cb04e903b458708c5ec25a03684750f1
Corey Magick <fatheranarchy@programmer.net>
2026-06-18 23:42:56 -0500

    udated

--- app/login.tsx
+++
@@ -1,2 +0,0 @@
-// This file is deprecated - login has been removed
-// The app is now a shared device interface with no authentication
--- hooks/use-local-auth.ts
+++
@@ -1,2 +0,0 @@
-// This file is deprecated - the app no longer uses authentication
-// All functionality is now shared device access with no login
